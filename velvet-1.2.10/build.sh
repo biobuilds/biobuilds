e60dee867438fb447ddfed438ca56081a5f78213
Cheng H. Lee <cheng.lee@lab7.io>
2015-09-23 13:45:13 -0500

    Updated conda recipe for velvet 1.2.10
    Modified velvet to support building oases with conda by:
    - Extending max k-mer length to match oases default;
    - Creating libvelvet.a so we don't need to build velvet to build oases

--- velvet-1.2.10/build.sh
+++ velvet-1.2.10/build.sh
@@ -14,9 +14,20 @@ LDFLAGS="${LDFLAGS} $(pkg-config --libs zlib)"
 
 # Build
 env CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" \
-    make -j${BB_MAKE_JOBS} OPENMP=1 LONGSEQUENCES=1 bin
+    make -j${BB_MAKE_JOBS} OPENMP=1 \
+    MAXKMERLENGTH=64 LONGSEQUENCES=1 \
+    bin lib
 
 
 # Install
 [ -d "${PREFIX}/bin" ] || mkdir -p "${PREFIX}/bin"
+[ -d "${PREFIX}/lib" ] || mkdir -p "${PREFIX}/lib"
+[ -d "${PREFIX}/include/velvet" ] || mkdir -p "${PREFIX}/include/velvet"
 cp -p velvetg velveth "${PREFIX}/bin"
+cp -p libvelvet.a "${PREFIX}/lib"
+cp -p src/binarySequences.h src/concatenatedGraph.h \
+    src/correctedGraph.h src/dfibHeap.h src/fibHeap.h src/globals.h \
+    src/graph.h src/graphReConstruction.h src/graphStats.h src/kmer.h \
+    src/locallyCorrectedGraph.h src/passageMarker.h src/readSet.h \
+    src/recycleBin.h src/scaffold.h src/tightString.h src/utility.h \
+    "${PREFIX}/include/velvet"
