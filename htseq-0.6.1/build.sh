cd96694b5847443da3f077c1fa79875fd00571bc
Cheng H. Lee <cheng.lee@lab7.io>
2015-11-19 16:49:41 -0600

    conda recipe for HTSeq 0.6.1

---
+++ htseq-0.6.1/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
+[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
+[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
+CFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
+
+env CFLAGS="${CFLAGS}" \
+    "$PYTHON" setup.py install
