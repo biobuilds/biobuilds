3dfe47f6fdcf71801c036d3d2bb26cca10e53ec2
Cheng H. Lee <cheng.lee@lab7.io>
2015-12-01 17:44:08 -0600

    Include build ids in biobuilds=2015.11 requirements
    Tweaked the "requirements" section of the BioBuilds 2015.11 recipe's
    meta.yaml to include the (conda) build id as well as the version for
    each package. This lets us continue developing build recipes for
    existing packages while ensuring that users installing the 2015.11
    "reference release" will always get exactly the same set of binaries
    that were available at release time. (Part of our "let's use conda for
    managing reproducible scientific computing environments" messaging).

--- biobuilds/2015.11/build.sh
+++ biobuilds/2015.11/build.sh
@@ -4,4 +4,4 @@
 # strings (relating to the Python, Numpy, and R releases). Since a given
 # BioBuilds release fixes (pins) these, there's little reason to attach
 # such a long build string to the release package.
-echo "0" > __conda_buildstr__.txt
+echo "${PKG_BUILDNUM}" > __conda_buildstr__.txt
