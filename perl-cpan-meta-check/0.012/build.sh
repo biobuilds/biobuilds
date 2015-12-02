e0491c22dea012a3b447ceb3b58f0e07b66223c2
Cheng H. Lee <cheng.lee@lab7.io>
2015-12-02 10:11:13 -0600

    Don't remove manpages from Perl packages
    Doing so doesn't actually save any meaningful amount of space (typically
    only a few kiB) and only makes it harder for users who might want to
    program using the Perl modules installed in their conda environments.

--- perl-cpan-meta-check/0.012/build.sh
+++ perl-cpan-meta-check/0.012/build.sh
@@ -5,4 +5,3 @@ perl Makefile.PL INSTALLDIRS=site
 make
 make test
 make install
-rm -rf "${PREFIX}/man"  "${PREFIX}/share/man"
--- perl-test-warnings/0.021/build.sh
+++ perl-test-warnings/0.021/build.sh
@@ -4,4 +4,3 @@ perl Makefile.PL INSTALLDIRS=site
 make
 make test
 make install
-rm -rf "${PREFIX}/man"  "${PREFIX}/share/man"
