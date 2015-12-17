0f582f5a0d9d2708b02e234a88654a2d011cbb87
Cheng H. Lee <cheng.lee@lab7.io>
2015-12-17 08:47:32 -0600

    Various improvements to sra-tools 2.5.6 recipe
    * Added basic tests
    * Added libmagic and hdf5 support
    * Use dynamic linking where possible; since we can control the RPATH,
      copying symbols into every binary needlessly wastes space. Resulting
      packages are ~10% the size of the statically linked versions.

--- sra-tools/2.5.6/build.sh
+++ sra-tools/2.5.6/build.sh
@@ -7,8 +7,9 @@ build_arch="$(uname -m)"
 # If available, pull in BioBuilds optimization flags
 [ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
 [ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
-CFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
-CXXFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
+export CFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
+export CXXFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 # Make sure various libraries are available to "make test"
 if [ "$build_os" == "Darwin" ]; then
@@ -36,11 +37,17 @@ find "libs/search" -name '*.s' | xargs rm -f
 # fixes libxml2 linking issue (see makefiles.patch for details).
 rm -rf "libs/ext/zlib" "libs/ext/bzip2"
 
+# Remove sources for libmagic and use conda-provided library instead
+rm -rf "libs/ext/magic"
+
 # TODO: figure out how to pass CFLAGS, LDFLAGS to this build system
 ./configure --prefix="${PREFIX}" \
     --build-prefix="${PWD}/local-build" \
     --with-ngs-sdk-prefix="${PREFIX}" \
     --with-xml2-prefix="${PREFIX}" \
+    --with-magic-prefix="${PREFIX}" \
+    --with-hdf5-prefix="${PREFIX}" \
+    LDFLAGS="${LDFLAGS}" \
     2>&1 | tee configure.log
 
 make 2>&1 | tee build.log
@@ -49,7 +56,7 @@ make 2>&1 | tee build.log
 # moving quite a lot of data (650+ MB) across the Internet.
 #env LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" make test 2>&1 | tee test.log
 
-#make install
+make install
 
 
 #--------------------------------------------------------------------------
@@ -64,13 +71,16 @@ rm -rf "local-build"
     --build-prefix="${PWD}/local-build" \
     --with-ngs-sdk-prefix="${PREFIX}" \
     --with-xml2-prefix="${PREFIX}" \
+    --with-magic-prefix="${PREFIX}" \
+    --with-hdf5-prefix="${PREFIX}" \
     --with-ncbi-vdb-sources="${PWD}/../ncbi-vdb-${PKG_VERSION}" \
     --with-ncbi-vdb-build="${PWD}/../ncbi-vdb-${PKG_VERSION}/local-build" \
+    LDFLAGS="${LDFLAGS}" \
     2>&1 | tee configure.log
 
 make 2>&1 | tee build.log
 
-env LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" make test 2>&1 | tee test.log
+make test 2>&1 | tee test.log
 
 make install
 
@@ -78,4 +88,7 @@ make install
 #--------------------------------------------------------------------------
 # miscellaneous build steps
 
-rm -f "${PREFIX}/lib64"
+# Clean up the install
+cd "${PREFIX}"
+rm -f lib64                 # don't need lib64 symlink
+rm -rf include lib/*.a*     # don't need development headers & libraries
---
+++ sra-tools/2.5.6/run_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Testing basic set of commands to downloads and outputs FASTQ files; based on
+# tutorial at <https://github.com/ncbi/sra-tools/wiki/Download-On-Demand>.
+
+RUN_ID="ERR1110123"
+
+abort() {
+    echo "ERROR: ${1:-UNKNOWN!}" >&2
+    exit 1
+}
+
+[ -d "$PWD/ncbi" ] && rm -rf "$PWD/ncbi"
+vdb-config -s "/repository/user/main/public/root=${PWD}/ncbi/public"
+
+# Limit to 2-MB download so tests don't take forever to run
+prefetch -X 2048 "$RUN_ID"
+[ "$(srapath $RUN_ID)" == "${PWD}/ncbi/public/sra/${RUN_ID}.sra" ] || \
+    abort "$RUN_ID.sff downloaded into wrong directory!"
+
+sff-dump "$RUN_ID"
+[ -f "${RUN_ID}.sff" ] || abort "${RUN_ID}.sff not found!"
+
+fastq-dump "$RUN_ID"
+[ -f "${RUN_ID}.fastq" ] || abort "${RUN_ID}.fastq not found!"
+
+echo -e "\n*** Verifying checksums ***"
+cat <<EOF |
+6a0f86e6bcc4c798bfa4aae071e2e800  ${RUN_ID}.fastq
+42b7024f7a21e4c84773b56a9640e81d  ${RUN_ID}.sff
+EOF
+md5sum -c -
