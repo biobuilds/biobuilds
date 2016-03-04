4246a384ff8c809b46bc96613a2225eae86d1d8d
Cheng H. Lee <cheng.lee@lab7.io>
2016-03-04 15:43:39 -0600

    Pin libgd version requirement for perl-gd
    Explicitly request version 2.1.0 to ensure maximal compatibility with
    existing tools; need to avoid grabbing 2.1.1 from "defaults" channel
    because it breaks existing OS X packages (see issue #5).
    
    (Also modified the recipe slightly to remove obsolete scripts.)

--- perl-gd/2.56/build.sh
+++ perl-gd/2.56/build.sh
@@ -10,3 +10,8 @@ perl Build.PL
 ./Build
 ./Build test
 ./Build install --installdirs site
+
+# Remove obsolete scripts (i.e., the ones that the README tells us not to run)
+cd "${PREFIX}"
+rm -f bin/bdf2gdfont.PLS bin/cvtbdf.pl bin/README \
+    share/man/man1/bdf2gdfont.PLS.1
