be92986fb11fbab6b47c0b14fbecdfae135e7114
Cheng H. Lee <cheng.lee@lab7.io>
2016-03-18 13:55:06 -0500

    Update veclib-headers to version 1.0.2
    Adds additional functions and performance improvements needed to support
    various BioBuilds packages on ppc64le (little-endian POWER8).

--- veclib-headers/1.0/build.sh
+++ veclib-headers/1.0/build.sh
@@ -5,5 +5,5 @@ if [ `uname -m` != 'ppc64le' ]; then
     exit 1
 fi
 [ -d "$PREFIX/include/veclib" ] || mkdir -p "$PREFIX/include/veclib"
-cp -p *.h "${PREFIX}/include/veclib"
-cp -p VERSION.TXT "${PREFIX}/include/veclib"
+cp -p include/*.h "${PREFIX}/include/veclib"
+echo "Version: veclib ${PKG_VERSION}" > "${PREFIX}/include/veclib/VERSION.TXT"
