b21a79752f37d44aaf3049a4f324c23789f5c12b
Cheng H. Lee <cheng.lee@lab7.io>
2016-03-21 18:55:49 -0500

    Recipe for chimerascan 0.4.5a

---
+++ chimerascan/0.4.5a/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Compile configuration
+command -v pkg-config >/dev/null || \
+    { echo "Could not find 'pkg-config' command" >&2; exit 1; }
+
+[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
+[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
+[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
+CFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
+CFLAGS="${CFLAGS} $(pkg-config --cflags-only-I zlib)"
+LDFLAGS="${LDFLAGS} $(pkg-config --libs-only-L zlib)"
+
+## Build and install
+env CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" \
+    "$PYTHON" setup.py install
