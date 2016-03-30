78f48c2e9559f19851fc1dd0432e4e1428199dba
Cheng H. Lee <cheng.lee@lab7.io>
2016-03-30 14:28:39 -0500

    Recipes for SnpEff and SnpSift 4.2

---
+++ snpeff/4.2/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SHARE_DIR="${PREFIX}/share/${PKG_NAME}"
+mkdir -p "${PREFIX}/bin" "${SHARE_DIR}"
+
+sed -i.bak "s:^data.dir =.*$:data.dir = ${SHARE_DIR}/data:" snpEff.config
+
+install -m 0644 snpEff.jar snpEff.config "${SHARE_DIR}"
+
+# Create launcher script in ${PREFIX}/bin
+cat >"${PREFIX}/bin/snpEff" <<EOF
+#!/bin/bash
+
+# Make sure 'java' is available through the user's \$PATH
+command -v java 1>/dev/null 2>/dev//null || \\
+    { echo 'ERROR: Could not find 'java' in your \$PATH.' >&2; exit 1; }
+
+# Assuming "java -version" formats information like the Oracle JRE
+java_minor_ver=\$(java -version 2>&1 | head -n1 | awk -F. '{print \$2;}')
+test \$java_minor_ver -lt 7 2>/dev/null; test_result=\$?
+if [ \$test_result -eq 0 ]; then
+    echo 'ERROR: Java 1.7 or later must be installed to use snpEff.' >&2;
+    exit 1
+elif [ \$test_result -ne 1 ]; then
+    echo 'ERROR: Could not determine Java version.' >&2
+    exit 1
+fi
+
+exec java -jar "${SHARE_DIR}/snpEff.jar" \$@
+EOF
+chmod 0755 "${PREFIX}/bin/snpEff"
