b3f1885538b4468f966682fbd60795339a044ea6
Cheng H. Lee <cheng.lee@lab7.io>
2016-10-10 13:55:39 -0500

    Recipe for jansson 2.9
    C library for handling JSON data; needed as a bedops dependency.

---
+++ jansson/2.9/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
+[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
+[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
+CFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
+
+env CFLAGS="${CFLAGS}" \
+    ./configure --prefix="$PREFIX"
+make -j${BB_MAKE_JOBS}
+make test
+make install
