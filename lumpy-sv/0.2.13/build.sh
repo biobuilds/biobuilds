cfcde6f1d54294c7012d8302c9b7f007e7436a6f
Cheng H. Lee <cheng.lee@lab7.io>
2016-11-02 13:10:07 -0500

    Recipe for lumpy-sv 0.2.13

---
+++ lumpy-sv/0.2.13/build.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#set -e -x
+set -e
+set -o pipefail
+
+## Configure
+build_os=$(uname -s)
+
+[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
+[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
+[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
+CXXFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
+CXXFLAGS="${CXXFLAGS} -fsigned-char"
+
+if [ "$build_os" == 'Darwin' ]; then
+    MACOSX_VERSION_MIN=10.8
+    CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+
+    # Must use the same 'stdlib' option as bamtools, or build will fail with
+    # multiple undefined symbols errors.
+    CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+    LDFLAGS="${LDFLAGS} -stdlib=libc++"
+fi
+
+CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
+CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include/bamtools"
+LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+# Additional flags in upstream Makefile that we squash when passing the
+# "CXXFLAGS" argument to "make"
+CXXFLAGS="${CXXFLAGS} -D_FILE_OFFSET_BITS=64 -fPIC"
+
+BIN_DIR="${PREFIX}/bin"
+LIBEXEC_DIR="${PREFIX}/libexec/${PKG_NAME}-${PKG_VERSION}"
+
+# Removed 3rd-party sources so we don't accidentally use them instead of the
+# versions provided by BioBuilds
+rm -rf src/utils/BamTools src/utils/sqlite3
+
+
+# Build
+rm -rf bin obj
+find . -name '*.o' | xargs rm -f
+
+make -j${BB_MAKE_JOBS} \
+    CXXFLAGS="${CXXFLAGS}" \
+    LDFLAGS="${LDFLAGS}" \
+    2>&1 | tee build.log
+
+## Install
+PREFIX_BIN="${PREFIX}/bin"
+PREFIX_ETC="${PREFIX}/etc"
+PREFIX_LIBEXEC="${PREFIX}/libexec/${PKG_NAME}-${PKG_VERSION}"
+
+install -d "${PREFIX_BIN}" "${PREFIX_ETC}" "${PREFIX_LIBEXEC}"
+
+install -m 0755 bin/lumpy "${PREFIX}/bin"
+sed "
+    s@^CONFIG=.*\$@CONFIG=${PREFIX_ETC}/lumpyexpress.config@;
+    s@=.*/bamkit/@${PREFIX_BIN}/@;
+    " < bin/lumpyexpress > "${PREFIX_BIN}/lumpyexpress"
+chmod 0755 "${PREFIX_BIN}/lumpyexpress"
+
+# Disable the 'errexit' (-e) and 'pipefail' shell options so "grep -qv" in the
+# following "for" loop doesn't cause this build script to fail.
+set +e +o pipefail
+
+# Install only those support scripts that are either required by the binaries
+# or mention in the documentation (README.md)
+for path in `find scripts -type f`; do
+    fn=`basename "$path"`
+
+    find README.md bin scripts src -type f | \
+        xargs egrep -niH "$fn" | \
+        grep -qv -- "^${path}"
+
+    if [ $? -eq 0 ]; then
+        dest="${PREFIX_LIBEXEC}/${fn}"
+        sed -n "
+            1s@^#!.*[ /]\(.*\)\$@#!${PREFIX}/bin/\1@p;
+            2,\$p;
+        " < "$path" > "$dest"
+        chmod 0755 "$dest"
+    fi
+done
+set -e -o pipefail
+
+# Config file
+cat >"${PREFIX_ETC}/lumpyexpress.config" <<EOF
+LUMPY_HOME=${PREFIX_LIBEXEC}
+
+LUMPY=${PREFIX_BIN}/lumpy
+SAMBLASTER=${PREFIX_BIN}/samblaster
+SAMBAMBA=
+SAMTOOLS=${PREFIX_BIN}/samtools
+PYTHON=${PREFIX_BIN}/python
+
+PAIREND_DISTRO=${PREFIX_LIBEXEC}/pairend_distro.py
+BAMGROUPREADS=${PREFIX_BIN}/bamgroupreads.py
+BAMFILTERRG=${PREFIX_BIN}/bamfilterrg.py
+BAMLIBS=${PREFIX_BIN}/bamlibs.py
+EOF
