8cdb6cd9bb7c408c4d3417937d01c563bc1b6f11
Cheng H. Lee <cheng.lee@lab7.io>
2016-11-03 14:57:17 -0500

    Rebuild autoconf 2.69 to support linux-ppc64le
    Just needed to update "config.guess" and "config.sub" so autoconf's
    ./configure recognized the little-endian POWER (ppc64le) architecture.

--- autoconf/2.69/build.sh
+++ autoconf/2.69/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+cp -fv "${PREFIX}/share/autoconf/config.guess" build-aux/config.guess
+cp -fv "${PREFIX}/share/autoconf/config.sub" build-aux/config.sub
+
 ./configure --prefix="$PREFIX"
 make
 make install
-
