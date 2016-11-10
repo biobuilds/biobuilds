77473c482dab0e1bc0fd7e73e57f71a266642964
Cheng H. Lee <cheng.lee@lab7.io>
2016-11-10 11:03:17 -0500

    Docker build containers: bump conda & conda-build versions
    Match versions that will be available on linux-ppc64le when BioBuilds
    2016.11 is released; this version bump requires us to work around:
    
    - "conda build" now creating build environments in subdirectories of
      $CONDA_BLD_PATH instead of /tmp/.conda, which can break executables
      like gcc through symlink-related issues.
    
    - "conda" insisting on writing or hard linking to files in the install
      prefix's (/opt/miniconda) "pkgs" and "envs" subdirectories, even when
      the user does not have ownership or write permissions.
    
    Workaround basically involves running the "conda build" process as the
    container's root user, followed by copying the built packages into
    "/src/conda-bld" and setting the file ownerships. NOTE: this means build
    containers should no longer be started using the "docker run" "--user"
    option; the "./docker-build.sh" script has been updated to reflect this.

--- utils/build-container/conda-build.sh
+++ utils/build-container/conda-build.sh
@@ -21,8 +21,60 @@ done
 cd "/src"
 source bb-build-flags.env ${BB_MAKE_JOBS:-1}
 
-if [ $shell -eq 0 ]; then
-    conda build $@
-else
+# If requested, drop to an interactive shell instead running "conda build"
+if [ $shell -eq 1 ]; then
     exec /bin/bash -i
 fi
+
+CONDA_PLATFORM=`conda info | grep -i 'platform :' | awk '{print $3;}'`
+
+# Now that "conda build" creates environments in $CONDA_BLD_PATH, we need to
+# avoid using "/src/conda-bld" as the build root directory; doing so would
+# force us to use expensive file copies when creating build environments, as
+# cross-device hard links aren't supported and symlinks breaks things like gcc.
+export CONDA_BLD_PATH=/tmp/conda-bld
+mkdir -p "${CONDA_BLD_PATH}"
+
+
+# Use local copies of existing BioBuilds packages to reduce network traffic
+if [ "/src/conda-bld/${CONDA_PLATFORM}" -o "/src/conda-bld/noarch" ]; then
+    channel_opts="-c file:///src/conda-bld"
+else
+    channel_opts=
+fi
+
+
+# conda sometimes tries to write in the install root's (/opt/miniconda) "pkgs"
+# or "envs" sub-directories, or tries to hard link to files within them, which
+# can trigger hard-to-diagnose failures if the container is run as a non-root
+# user (i.e., started with the "docker run" "-u" option). Since it's not clear
+# which packages trigger such behavior enough to provide another workaround,
+# run "conda build" as "root" using the container's "local" filesystem (see
+# above comments for the reasons why), copy the built packages into the correct
+# "/src/conda-bld" subdirectory, then call chown to "fix" the files' ownership.
+DEST_UID=`stat -c '%u' /src`
+DEST_GID=`stat -c '%g' /src`
+
+set -e -x
+touch /tmp/build_timestamp
+conda build $channel_opts $@
+
+for d in ${CONDA_PLATFORM} noarch; do
+    src_dir="${CONDA_BLD_PATH}/${d}"
+    dest_dir="/src/conda-bld/${d}"
+
+    if [ ! -d "$dest_dir" ]; then
+        mkdir -p "/src/conda-bld/${d}"
+        chown ${DEST_UID}:${DEST_GID} "/src/conda-bld/${d}"
+    fi
+
+    # Use "find" instead of globbing to prevent script failure in case no
+    # package files were placed inside this ${src_dir}.
+    find ${src_dir} -type f -name '*.tar.bz2' | \
+        xargs -I% cp -pv % ${dest_dir}
+
+    conda index ${dest_dir}
+
+    find "${dest_dir}" -type f -newer /tmp/build_timestamp | \
+        xargs chown ${DEST_UID}:${DEST_GID}
+done
