c6a4b4d2cc184a7a21136fada571733f66bad503
Cheng H. Lee <cheng.lee@lab7.io>
2017-04-29 00:39:07 -0500

    bwa 0.7.15: add "opt" feature support
    (Lab7 ticket: BIOBUILDS-272)

--- bwa/0.7.15/build.sh
+++ bwa/0.7.15/build.sh
@@ -3,31 +3,30 @@
 command -v pkg-config >/dev/null || \
     { echo "Could not find 'pkg-config' command" >&2; exit 1; }
 
+# Pull in the common BioBuilds build flags
+BUILD_ENV="${PREFIX}/share/biobuilds-build/build.env"
+if [[ ! -f "${BUILD_ENV}" ]]; then
+    echo "FATAL: Could not find build environment  configuration script!" >&2
+    exit 1
+fi
+source "${BUILD_ENV}" -v
+
 # Configure
-build_arch=$(uname -m)
-
-[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
-[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
-[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
-CFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
-
-if [ "$build_arch" == "ppc64le" ]; then
+if [ "$BUILD_ARCH" == "ppc64le" ]; then
     # Should be provided by the "veclib-headers" package
     [ -d "${PREFIX}/include/veclib" ] || \
         { echo "ERROR: could not find veclib headers" >&2; exit 1; }
     CFLAGS="${CFLAGS} -I${PREFIX}/include/veclib"
     CFLAGS="${CFLAGS} -fsigned-char"
-elif [ "$build_arch" != "x86_64" ]; then
+elif [ "$BUILD_ARCH" != "x86_64" ]; then
     echo "ERROR: Unsupported architecture '$build_arch'" >&2
     exit 1
 fi
 
-CFLAGS="${CFLAGS} $(pkg-config --cflags zlib)"
-LDFLAGS="${LDFLAGS} $(pkg-config --libs zlib)"
-
 # Build
-env CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" \
-    make -j${BB_MAKE_JOBS} all
+env CC="${CC}" CFLAGS="${CFLAGS}" \
+    LD="${LD}" LDFLAGS="${LDFLAGS}" \
+    make -j${MAKE_JOBS} all
 
 # Install
 [ -d "${PREFIX}/bin" ] || mkdir "${PREFIX}/bin"
