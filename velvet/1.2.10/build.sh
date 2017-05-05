1950f33502dbaea17d6ebb242901e4ca78cabe78
Cheng H. Lee <cheng.lee@lab7.io>
2017-05-05 17:51:24 -0500

    velvet 1.2.10: add "opt" feature support
    (Lab7 ticket: BIOBUILDS-273)

--- velvet/1.2.10/build.sh
+++ velvet/1.2.10/build.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-command -v pkg-config >/dev/null || \
-    { echo "Could not find 'pkg-config' command" >&2; exit 1; }
-
 # Configure
-[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
-[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
-[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
-CFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
-CFLAGS="${CFLAGS} $(pkg-config --cflags zlib)"
-LDFLAGS="${LDFLAGS} $(pkg-config --libs zlib)"
+# Pull in the common BioBuilds build flags
+BUILD_ENV="${PREFIX}/share/biobuilds-build/build.env"
+if [[ ! -f "${BUILD_ENV}" ]]; then
+    echo "FATAL: Could not find build environment  configuration script!" >&2
+    exit 1
+fi
+source "${BUILD_ENV}" -v
 
 
 # Build
 env CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" \
-    make -j${BB_MAKE_JOBS} OPENMP=1 \
+    make -j${MAKE_JOBS} OPENMP=1 \
     MAXKMERLENGTH=64 LONGSEQUENCES=1 \
     bin lib
 
