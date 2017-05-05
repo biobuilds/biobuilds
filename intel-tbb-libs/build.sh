13a649b23b604ba0388a292d78fbe71d6f52fdb1
Cheng H. Lee <cheng.lee@lab7.io>
2017-05-05 15:07:20 -0500

    Recipe for (re)packaging Intel TBB libs
    Pre-built versions of the TBB (Thread Building Blocks) runtime libraries
    provided directly by Intel. Meant for use with BioBuilds x86_64 Linux
    packages that have the "opt" feature enabled (i.e., those built using
    Intel Parallel Studio XE).
    
    NOTE: Non-"opt" packages should use the "tbb" package, which contains
    versions of these libraries built from the open-source release.

---
+++ intel-tbb-libs/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o pipefail
+
+# Match the default compiler used in the x86_64 Linux build container
+: ${GCC_VER:=4.4}
+
+BASE_DIR="opt/intel/compilers_and_libraries_${PKG_VERSION}"
+LIB_DIR="${BASE_DIR}/linux/tbb/lib/intel64/gcc${GCC_VER}"
+
+rpm2cpio rpm/intel-tbb-common-*.rpm | cpio -idm
+rpm2cpio rpm/intel-tbb-libs-*.rpm | cpio -idm
+if [ ! -d "${LIB_DIR}" ]; then
+    echo "Could not find expected library directory!" >&2
+    exit 1
+fi
+
+mkdir -p "${PREFIX}/lib"
+cd "${LIB_DIR}"
+cp -fvp libtbb*.so.* "${PREFIX}/lib"
+
+cd "${PREFIX}/lib"
+rm -f *_debug.so.*
+for fn in libtbb*.so.*; do
+    ln -s "${fn}" "${fn/.so*/.so}"
+done
