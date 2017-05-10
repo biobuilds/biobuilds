7086ca616151b11ef6457b8ff018d7a5b6eb031f
Cheng H. Lee <cheng.lee@lab7.io>
2017-05-10 16:58:02 -0500

    fasta 36.3.8e: add "opt" feature support
    (Lab7 ticket: BIOBUILDS-289)

--- fasta/36.3.8e/build.sh
+++ fasta/36.3.8e/build.sh
@@ -1,37 +1,56 @@
 #!/bin/bash
 set -o pipefail
 
-# Configure
-build_os=$(uname -s)
-build_arch=$(uname -m)
+## Configure
 
-[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS="-m64"
-[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS="-O3"
-[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
-CFLAGS="${CFLAGS} ${BB_OPT_FLAGS}"
-CFLAGS="${CFLAGS} -I${PREFIX}/include"
-LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+# Pull in the common BioBuilds build flags
+BUILD_ENV="${PREFIX}/share/biobuilds-build/build.env"
+if [[ ! -f "${BUILD_ENV}" ]]; then
+    echo "FATAL: Could not find build environment configuration script!" >&2
+    exit 1
+fi
+source "${BUILD_ENV}" -v
 
-if [ "$build_arch" == "ppc64le" ]; then
+# Architecture-specific tweaks
+if [ "$BUILD_ARCH" == "ppc64le" ]; then
     makefile="../make/Makefile.ppc64le.gnu"
     # Should be provided by the "veclib-headers" package
     [ -d "${PREFIX}/include/veclib" ] || \
         { echo "ERROR: could not find veclib headers" >&2; exit 1; }
     CFLAGS="$CFLAGS -I${PREFIX}/include/veclib"
-elif [ "$build_arch" == "x86_64" ]; then
+    CFLAGS="$CFLAGS -fsigned-char"
+elif [ "$BUILD_ARCH" == "x86_64" ]; then
     makefile="../make/Makefile.linux64_sse2"
 else
     echo "ERROR: Unsupported architecture '$build_arch'" >&2
     exit 1
 fi
 
-# Build
-cd "${SRC_DIR}/src"
-env CC="gcc ${BB_ARCH_FLAGS}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" \
-    make -j${BB_MAKE_JOBS} -f "$makefile"
-cd "${SRC_DIR}/test"
+# Tweaks for the Intel compiler
+if [[ "${CC}" == *"/bin/icc" ]]; then
+    # Be more conservative with floating-point optimizations in an attempt to
+    # match the results produced by the GCC-compiled version. Note, we haven't
+    # actually _shown_ that this has any meaningful effect, mostly because the
+    # binaries use randomization in a way that makes it difficult to reliably
+    # reproduce outputs (e.g., we can't set the seed to use).
+    CFLAGS="${CFLAGS/-fp-model fast=1/-fp-model precise}"
+    CFLAGS="${CFLAGS/-no-prec-div/}"
+fi
 
-# Install
+
+## Build
+cd "${SRC_DIR}/src"
+env CC="${CC}" CFLAGS="${CFLAGS}" \
+    LDFLAGS="${LDFLAGS}" \
+    make -j${MAKE_JOBS} -f "$makefile" \
+    2>&1 | tee build.log
+
+# Not running supplied tests due to missing $FASTLIBS
+#cd "${SRC_DIR}/test"
+#./test.sh
+
+
+## Install
 INSTALL_BIN="${PREFIX}/bin"
 INSTALL_SHARE="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
 
