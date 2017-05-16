2e958455f8ed10ef4993e175cc64d7188bf0245d
Cheng H. Lee <cheng.lee@lab7.io>
2017-05-15 20:31:09 -0500

    clustalomega 1.2.4: added "opt" feature support
    (Lab7 ticket: BIOBUILDS-305)

--- clustalomega/1.2.4/build.sh
+++ clustalomega/1.2.4/build.sh
@@ -11,12 +11,22 @@ if [[ ! -f "${BUILD_ENV}" ]]; then
 fi
 source "${BUILD_ENV}" -v
 
+# Additional tweaks for Intel Parallel Studio
+if [[ "${CC}" == *"/bin/icc" ]]; then
+    # If using conda's gcc, help `icpc` find libstdc++ include files
+    if [ -d "${PREFIX}/gcc" ]; then
+        export GXX_INCLUDE="${PREFIX}/gcc/include"
+        CXXFLAGS="${CXXFLAGS} -I${GXX_INCLUDE}/c++"
+        CXXFLAGS="${CXXFLAGS} -I${GXX_INCLUDE}/c++/${BUILD_ARCH}-unknown-${BUILD_OS}-gnu"
+    fi
+fi
+
 # Update autoconf files for ppc64le detection
 cp -f "${PREFIX}/share/autoconf/config.guess" "${SRC_DIR}/config.guess"
 cp -f "${PREFIX}/share/autoconf/config.sub" "${SRC_DIR}/config.sub"
 
 env CC="${CC}" CFLAGS="${CFLAGS}" \
-    CXX="${CXX}" CXXFLAGS="${CFLAGS}" \
+    CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
     LDFLAGS="${LDFLAGS}" \
     ./configure --prefix="${PREFIX}" \
     2>&1 | tee config.log
