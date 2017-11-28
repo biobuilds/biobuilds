3c84763c4adb749805925e533f11886d42052b8f
Cheng H. Lee <cheng.lee@lab7.io>
2017-11-27 18:29:28 -0600

    Rebuild delly 0.7.6 for r2017.11 compatibility
    Making sure delly works with the BioBuilds 2017.11 Boost packages.
    
    (Lab7 ticket: BIOBUILDS-372)

--- delly/0.7.6/build.sh
+++ delly/0.7.6/build.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 set -o pipefail
 
-# Using static libhts so a htslib upgrade in the conda environment
-# doesn't suddenly break our binaries.
-[ -f "${PREFIX}/lib/libhts.a" ] || \
-    { echo "ERROR: Could not find static libhts!" >&2; exit 1; }
-
 ## Configure
-build_arch=$(uname -m)
-build_os=$(uname -s)
 
-[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
-[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
-[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
-CXXFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
+# Pull in the common BioBuilds build flags
+BUILD_ENV="${PREFIX}/share/biobuilds-build/build.env"
+if [[ ! -f "${BUILD_ENV}" ]]; then
+    echo "FATAL: Could not find build environment configuration script!" >&2
+    exit 1
+fi
+source "${BUILD_ENV}" -v
 
-if [ "${build_arch}" == "ppc64le" ]; then
+if [ "${BUILD_ARCH}" == "ppc64le" ]; then
     CXXFLAGS="${CXXFLAGS} -fsigned-char"
 fi
 
-if [ "${build_os}" == 'Darwin' ]; then
-    # See meta.yaml for why we have to disable OpenMP on OS X
-    USE_OPENMP=0
+if [ "${BUILD_OS}" == 'darwin' ]; then
+    USE_OPENMP=1
 
     MACOSX_VERSION_MIN="10.8"
     CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
@@ -30,7 +25,7 @@ if [ "${build_os}" == 'Darwin' ]; then
     # Make sure we use the same C++ standard library as boost
     CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
     LDFLAGS="${LDFLAGS} -stdlib=libc++"
-elif [ "${build_os}" == 'Linux' ]; then
+elif [ "${BUILD_OS}" == 'linux' ]; then
     USE_OPENMP=1
 else
     echo "ERROR: Unsupported OS '$build_os'!" >&2
@@ -41,10 +36,19 @@ fi
 CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
 LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+# Make sure g++ knows that our Boost libraries were built with a pre-C++11 ABI.
+#
+# NOTE: we don't have a conditional here to check for g++ because conda's
+# linux-64 C++ compiler package sets $CXX to the binary named "*-c++", and we
+# don't want to put the additional effort into determining if said "c++" binary
+# is _really_ the GNU compiler.
+CXXFLAGS="${CXXFLAGS} -D_GLIBCXX_USE_CXX11_ABI=0"
+
 
 ## Build
-env CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" \
-    make -j${BB_MAKE_JOBS} PARALLEL=${USE_OPENMP} \
+env CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
+    LD="${LD}" LDFLAGS="${LDFLAGS}" \
+    make -j${MAKE_JOBS} PARALLEL=${USE_OPENMP} \
     LIBHTS="-lhts -lcurl -lcrypto -lssl" \
     2>&1 | tee build.log
 
