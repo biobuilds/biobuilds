ab68417d1969f70a4df386e6b5f832d07f327fb6
Cheng H. Lee <cheng.lee@lab7.io>
2017-11-27 19:28:13 -0600

    Rebuild scikit-bio 0.4.2 for r2017.11 compatibility
    Use the Anaconda toolchain and NumPy 1.13 to ensure compatibility with
    other packages in the upcoming BioBuilds 2017.11 release.
    
    (Lab7 ticket: BIOBUILDS-372)

--- scikit-bio/0.4.2/build.sh
+++ scikit-bio/0.4.2/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-env CFLAGS="${CFLAGS} -I${PREFIX}/include" \
-    $PYTHON setup.py install
+# Pull in the common BioBuilds build flags
+BUILD_ENV="${PREFIX}/share/biobuilds-build/build.env"
+if [[ ! -f "${BUILD_ENV}" ]]; then
+    echo "FATAL: Could not find build environment configuration script!" >&2
+    exit 1
+fi
+source "${BUILD_ENV}" -v
+
+export CC CFLAGS CXX CXXFLAGS LD LDFLAGS
+$PYTHON setup.py install
