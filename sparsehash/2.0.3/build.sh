3cfa2db1816c23de1809b427557229b15f9d3a57
Cheng H. Lee <cheng.lee@lab7.io>
2017-12-05 05:22:00 -0600

    Rebuild sparsehash 2.0.3 with Anaconda toolchain
    Mostly needed on OS X/macOS so clang++ doesn't barf on the
    <tr1/functional> include from our earlier build.

--- sparsehash/2.0.3/build.sh
+++ sparsehash/2.0.3/build.sh
@@ -13,12 +13,19 @@ source "${BUILD_ENV}" -v
 cp -fv "${PREFIX}/share/autoconf/config.guess" "config.guess"
 cp -fv "${PREFIX}/share/autoconf/config.sub" "config.sub"
 
+# Target the oldest C++ standard we expect to support; need to do this because
+# some of our compilers assume a newer standard (C++11 or C++14), but we want
+# to use this library to build software written in older C++ flavors.
+CXXFLAGS="-std=c++98 ${CXXFLAGS}"
+
 env CC="${CC}" CFLAGS="${CFLAGS}" \
     CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
     ./configure --prefix="${PREFIX}" \
-    --with-boost="${BOOST_PREFIX}" \
     2>&1 | tee configure.log
 
 make -j${MAKE_JOBS} V=1 2>&1 | tee build.log
 make check 2>&1 | tee check.log
 make install
+
+# No need to keep the documentation
+rm -rf ${PREFIX}/share/doc
