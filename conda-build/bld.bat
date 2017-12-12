2d34524e10771d5a3f658dc6cf67972aa8dc0f83
Cheng H. Lee <cheng.lee@lab7.io>
2017-12-12 15:27:05 -0600

    Update to conda-build 3.1.0
    Updating our (ppc64le) build containers as part of the process of
    getting the Anaconda (gcc 7.x) toolchain working on POWER. Building
    version 3.1.0 first (rather than jumping straight to the latest version)
    to ensure the conda-build packages we will ultimately install have the
    simplified hashing scheme introduced in version 3.1.0.
    
    (Lab7 ticket: BIOBUILDS-381)

--- conda-build/bld.bat
+++ conda-build/bld.bat
@@ -1,8 +1,8 @@
 python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
 
-copy bdist_conda.py "%PREFIX%\Lib\distutils\command\"
+del %SCRIPTS%\conda-init
 if errorlevel 1 exit 1
 
-del /f /q "%PREFIX%\Scripts\conda"
+copy bdist_conda.py %PREFIX%\Lib\distutils\command\
 if errorlevel 1 exit 1
--- conda-build/build.sh
+++ conda-build/build.sh
@@ -3,5 +3,3 @@
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
 
 cp bdist_conda.py "${PREFIX}/lib/python${PY_VER}/distutils/command/"
-
-rm -f "${PREFIX}/bin/conda"
