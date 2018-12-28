31f0db465a08822cb26018e179dde484b6ec8b1c
Cheng H. Lee <cheng.lee@l7informatics.com>
2018-12-28 17:31:28 -0600

    Rebuild bzip2, zlib with updated Anaconda toolchain
    Fixes `libgcc-ng` pinning issues that were causing `conda build` to
    install the ICC- or AT-compiled (i.e., "opt") versions of these packages
    in build environments using the newer Anaconda compiler packages. This,
    in turn, could inject unwanted dependencies on vendor runtime libraries
    in the standard (i.e., non-"opt") builds of various BioBuilds packages.
    
    (L7 Informatics ticket: BIOBUILDS-394)

--- bzip2/1.0.6/build.sh
+++ bzip2/1.0.6/build.sh
@@ -1,32 +1,46 @@
 #!/bin/sh
 
 # Pull in the common BioBuilds build flags
-BUILD_ENV="${PREFIX}/share/biobuilds-build/build.env"
+BUILD_ENV="${BUILD_PREFIX}/share/biobuilds-build/build.env"
 if [[ ! -f "${BUILD_ENV}" ]]; then
     echo "FATAL: Could not find build environment configuration script!" >&2
     exit 1
 fi
 source "${BUILD_ENV}" -v
 
-# toolchain flags + bzip flags + fpic
-export CFLAGS="${CFLAGS} -Wall -Winline -D_FILE_OFFSET_BITS=64 -fPIC"
-USED_CC=${GCC:-${CC}}
-make install PREFIX="${PREFIX}" \
-    CFLAGS="${CFLAGS}" CC="${USED_CC}" AR="${AR}"
+# Make sure we generate position independent code so `libbz2.a` can be used by
+# other recipes to generate shared libraries (.so/.dylib).
+CFLAGS="${CFLAGS} -fPIC"
+
+# Replace the upstream flags we'd squash with ours
+CFLAGS="${CFLAGS} -Wall -Winline -D_FILE_OFFSET_BITS=64"
+
+make install V=1 \
+    PREFIX="${PREFIX}" \
+    CC="${CC}" \
+    AR="${AR}" \
+    RANLIB="${RANLIB}" \
+    CFLAGS="${CFLAGS}" \
+    LDFLAGS="${LDFLAGS}"
 
 # build shared library
 case "$BUILD_OS" in
     "linux")
-        make -f Makefile-libbz2_so \
-            CFLAGS="${CFLAGS}" CC="${USED_CC}" AR="${AR}"
+        make -f Makefile-libbz2_so V=1 \
+            CC="${CC}" \
+            AR="${AR}" \
+            RANLIB="${RANLIB}" \
+            CFLAGS="${CFLAGS}" \
+            LDFLAGS="${LDFLAGS}"
         ln -s libbz2.so.${PKG_VERSION} libbz2.so
         cp -fv -d libbz2.so* ${PREFIX}/lib/
         ;;
     "darwin")
-        ${USED_CC} \
+        "${CC}" \
             -shared -Wl,-install_name \
             -Wl,libbz2.dylib \
             -o libbz2.${PKG_VERSION}.dylib \
+            ${LDFLAGS} \
             blocksort.o huffman.o crctable.o randtable.o compress.o decompress.o bzlib.o
         cp -fv libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/
         ln -s libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/libbz2.dylib
--- zlib/1.2.8/build.sh
+++ zlib/1.2.8/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Pull in the common BioBuilds build flags
-BUILD_ENV="${PREFIX}/share/biobuilds-build/build.env"
+BUILD_ENV="${BUILD_PREFIX}/share/biobuilds-build/build.env"
 if [[ ! -f "${BUILD_ENV}" ]]; then
     echo "FATAL: Could not find build environment configuration script!" >&2
     exit 1
 fi
 source "${BUILD_ENV}" -v
+
+TEST_LDFLAGS="-L. libz.a"
 
 if [[ "${toolchain:-default}" == "intel-"* ]]; then
     IPP_ROOT="${PSXE_ROOT:-/opt/intel}/ipp"
@@ -26,10 +28,18 @@ if [[ "${toolchain:-default}" == "intel-"* ]]; then
     LDFLAGS="${LDFLAGS} ${IPP_LIB}/libippdc.a"
     LDFLAGS="${LDFLAGS} ${IPP_LIB}/libipps.a"
     LDFLAGS="${LDFLAGS} ${IPP_LIB}/libippcore.a"
+
+    TEST_LDFLAGS="${TEST_LDFLAGS} ${IPP_LIB}/libippdc.a"
+    TEST_LDFLAGS="${TEST_LDFLAGS} ${IPP_LIB}/libipps.a"
+    TEST_LDFLAGS="${TEST_LDFLAGS} ${IPP_LIB}/libippcore.a"
 fi
 
 ./configure --prefix="$PREFIX"
-make -j${MAKE_JOBS} shared
+make -j${MAKE_JOBS} ${VERBOSE_AT} shared
+make check TEST_LDFLAGS="${TEST_LDFLAGS}"
 make install
 
 rm -rf $PREFIX/share
+
+# Copy license file to the source directory so conda-build can find it.
+cp "${RECIPE_DIR}/license.txt" "${SRC_DIR}/license.txt"
