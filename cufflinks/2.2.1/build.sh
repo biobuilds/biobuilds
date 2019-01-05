472079414034aa8d547f37b32804ce3614cc7e8b
Cheng H. Lee <cheng.lee@l7informatics.com>
2019-01-05 02:06:18 -0600

    Rebuild cufflinks 2.2.1 with updated Anaconda toolchain
    (L7 Informatics ticket: BIOBUILDS-406)

--- cufflinks/2.2.1/build.sh
+++ cufflinks/2.2.1/build.sh
@@ -1,47 +1,53 @@
 #!/bin/bash
 set -o pipefail
 
-# Configure
-build_os=$(uname -s)
-
-[ "$BB_ARCH_FLAGS" == "<UNDEFINED>" ] && BB_ARCH_FLAGS=
-[ "$BB_OPT_FLAGS" == "<UNDEFINED>" ] && BB_OPT_FLAGS=
-[ "$BB_MAKE_JOBS" == "<UNDEFINED>" ] && BB_MAKE_JOBS=1
-CFLAGS="${CFLAGS} ${BB_ARCH_FLAGS} ${BB_OPT_FLAGS}"
-CFLAGS="${CFLAGS} $(pkg-config --cflags zlib)"
-CFLAGS="${CFLAGS} $(pkg-config --cflags eigen)"
-CXXFLAGS="${CFLAGS}"
-LDFLAGS="${LDFLAGS} $(pkg-config --libs zlib)"
-
-if [ "$build_os" == 'Darwin' ]; then
-    CFLAGS="${CFLAGS} -Wno-deprecated-register -Wno-unused-variable"
-    CXXFLAGS="${CXXFLAGS} -Wno-deprecated-register -Wno-unused-variable"
-
-    MACOSX_VERSION_MIN="10.8"
-    CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-
-    # Make sure we use the same C++ standard library as boost
-    # NOTE: need to set this CFLAGS as well, as ./configure and the resulting
-    # Makefile seem to ignore CXXFLAGS in some cases.
-    CFLAGS="${CFLAGS} -stdlib=libc++"
-    CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-    LDFLAGS="${LDFLAGS} -stdlib=libc++"
+## Configure
+BUILD_ENV="${BUILD_PREFIX}/share/biobuilds-build/build.env"
+if [[ ! -f "${BUILD_ENV}" ]]; then
+    echo "FATAL: Could not find build environment  configuration script!" >&2
+    exit 1
 fi
+source "${BUILD_ENV}" -v
 
-cp -f "${RECIPE_DIR}/config.guess" "${SRC_DIR}/build-aux/config.guess"
-cp -f "${RECIPE_DIR}/config.sub" "${SRC_DIR}/build-aux/config.sub"
+case "$HOST_OS" in
+    "darwin")
+        #CFLAGS="${CFLAGS} -Wno-deprecated-register -Wno-unused-variable"
+        #CXXFLAGS="${CXXFLAGS} -Wno-deprecated-register -Wno-unused-variable"
+        ;;
+    "linux")
+        # Need this, or the build may fail with an "undefined reference to
+        # symbol 'clock_gettime@@GLIBC_2.2.5'" error.
+        export LIBS="-lrt"
+        ;;
+    *)
+        echo "FATAL: unsupported operating system '$HOST_OS'" >&2
+        exit 1
+        ;;
+esac
+
+# Could do this using `pkg-config`, but that seems excessive...
+CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include/eigen3"
+
+# Update so `./configure` knows how to deal with newer systems
+cp -fv "${BUILD_PREFIX}/share/autoconf/config.guess" "build-aux/config.guess"
+cp -fv "${BUILD_PREFIX}/share/autoconf/config.sub" "build-aux/config.sub"
 
 [ -f Makefile ] && make distclean
-env CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" \
-    ./configure --prefix="${PREFIX}" \
-    --with-zlib="${PREFIX}" --with-boost="${PREFIX}" \
-    --with-eigen="${PREFIX}" --with-bam="${PREFIX}" \
+./configure --prefix="${PREFIX}" \
+    --with-zlib="${PREFIX}" \
+    --with-boost="${PREFIX}" \
+    --with-eigen="${PREFIX}" \
+    --with-bam="${PREFIX}" \
     2>&1 | tee configure.log
 
-# Build
-make -j${BB_MAKE_JOBS} 2>&1 | tee build.log
+# `./configure` above seems very insistent on adding `-I/usr/include` to our
+# compiler flags. Unfortunately, this horribly breaks our clang's (and possibly
+# gcc's) ability to compile files that use Boost (lots of "no member" errors
+# related to math functions), so we need to remove the flag from our Makefiles.
+sed -i.bak 's,-I/usr/include, ,g' Makefile src/Makefile
 
-# Install
+## Build
+make -j${MAKE_JOBS} 2>&1 | tee build.log
+
+## Install
 make install-strip
